e359e3ff54db64f7af61f225f54fca74889826ad
ramyapooja <41736592+ramyapooja@users.noreply.github.com>
2018-08-02 13:40:49 +0530

    Create range.java

---
+++ range.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+         Scanner sc=new Scanner(System.in);
+         int num=sc.nextInt();
+         int n=sc.nextInt();
+         int m=sc.nextInt();
+         if(num>n&&num<m)
+         System.out.println("yes");
+         else
+         System.out.println("no");
+         
+	}
+}
