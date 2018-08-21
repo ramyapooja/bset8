cec68bde3ea33c3a06afe782e45c9803adc67ba0
ramyapooja <41736592+ramyapooja@users.noreply.github.com>
2018-08-21 10:50:33 +0530

    Create palindrome1.java

---
+++ palindrome1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String a=sc.next();
+		String b="";
+		for(int i=a.length()-1;i>=0;i--)
+		{
+	               b=b+a.charAt(i);
+		}
+		if(a.equals(b))
+		{
+		  System.out.println("yes");
+		}
+		else
+		  System.out.println("no");
+	}
+}
